29cf727b2dec421348a6134f6a530e7492d31a4c
drieseng <gert.driesen@telenet.be>
2022-03-19 13:31:38 +0100

    Fix path to sftp module.

--- src/SshNetTests/Common/RemoteSshdConfigExtensions.cs
+++ src/SshNetTests/Common/RemoteSshdConfigExtensions.cs
@@ -10,11 +10,12 @@ namespace SshNetTests.Common
         {
             remoteSshdConfig.WithAuthenticationMethods(Users.Regular.UserName, DefaultAuthenticationMethods)
                             .WithChallengeResponseAuthentication(false)
+                            .WithKeyboardInteractiveAuthentication(false)
                             .WithLogLevel(LogLevel.Debug3)
                             .ClearHostKeyFiles()
                             .AddHostKeyFile(HostKeyFile.Rsa.FilePath)
                             .ClearSubsystems()
-                            .AddSubsystem(new Subsystem("sftp", "/usr/lib/openssh/sftp-server"))
+                            .AddSubsystem(new Subsystem("sftp", "/usr/lib/ssh/sftp-server"))
                             .ClearCiphers()
                             .ClearKeyExchangeAlgorithms()
                             .ClearHostKeyAlgorithms()
