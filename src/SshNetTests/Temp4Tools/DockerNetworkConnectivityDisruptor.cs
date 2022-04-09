e1ae7368cea09e609ae34003e44137a0beb30fc1
Igor Milavec <igor.milavec@gmail.com>
2022-04-09 10:34:51 +0200

    Refactor network disruption code into separate class hierarchy, make tests inconclusive if disruption fails

---
+++ src/SshNetTests/Temp4Tools/DockerNetworkConnectivityDisruptor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SshNet.TestTools.OpenSSH
+{
+    public sealed class DockerNetworkConnectivityDisruptor : NetworkConnectivityDisruptor
+    {
+        public DockerNetworkConnectivityDisruptor(string containerName)
+        {
+            ContainerName = containerName;
+        }
+
+        private string ContainerName { get; }
+
+        public override void Start()
+        {
+            var process = DockerCommand($"pause {ContainerName}");
+            if (!process.WaitForExit(30000))
+            {
+                process.Kill();
+                throw new ApplicationException("Failed to pause the 'sshnet' container: timed out.");
+            }
+            if (process.ExitCode != 0)
+            {
+                throw new ApplicationException($"Failed to pause the 'sshnet' container: status {process.ExitCode}.");
+            }
+        }
+
+        public override void End()
+        {
+            var process = DockerCommand($"unpause {ContainerName}");
+            if (!process.WaitForExit(30000))
+            {
+                process.Kill();
+                throw new ApplicationException("Failed to resume the 'sshnet' container: timed out.");
+            }
+            if (process.ExitCode != 0)
+            {
+                throw new ApplicationException($"Failed to resume the 'sshnet' container: status {process.ExitCode}.");
+            }
+        }
+
+        private static Process DockerCommand(string command)
+        {
+            return Process.Start(
+                new ProcessStartInfo
+                {
+                    FileName = "docker",
+                    Arguments = command,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                });
+        }
+    }
+}
